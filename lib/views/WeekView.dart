cd4ce3abd7feb8c8f61a2e8baee23e63600ce3e8
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-09-23 15:25:36 +0200

    fixed WeekView

--- lib/views/WeekView.dart
+++ lib/views/WeekView.dart
@@ -49,14 +49,12 @@ class WeekView extends StatefulWidget {
 }
 
 class _WeekViewState extends State<WeekView> {
-  List<Widget> _lessonsWidgets = [];
   List<String> _filteredSubjects = [];
   int nDays = -1;
 
   manageLessons() async {
     SettingUtils.getData("lessons").then((lessonsToFilter) {
-      Map<String, dynamic> decodedLessonsToFilter =
-          json.decode(lessonsToFilter);
+      Map<String, dynamic> decodedLessonsToFilter = json.decode(lessonsToFilter);
       decodedLessonsToFilter.removeWhere((key, value) => value == true);
 
       _filteredSubjects.addAll(decodedLessonsToFilter.keys);
@@ -65,7 +63,11 @@ class _WeekViewState extends State<WeekView> {
         map["lessons"] = sortedLessons;
         map["filteredSubjects"] = _filteredSubjects;
         compute(removeFilteredLessons, map).then(
-          (filteredLessons) => widget._lessons = filteredLessons,
+          (filteredLessons) => {
+            setState((){
+              widget._lessons = filteredLessons;
+            })
+          }
         );
       });
     });
@@ -83,66 +85,19 @@ class _WeekViewState extends State<WeekView> {
     super.initState();
   }
 
-  // void createLessonWidgets() {
-  //   String currentDayName = "";
-  //   List<Widget> l = [];
-  //
-  //   widget._lessons.forEach((lesson) {
-  //     if (lesson["nome_insegnamento"] != null &&
-  //         !_filteredSubjects.contains(lesson["nome_insegnamento"])) {
-  //       String dayName = widget._nomeGiorni[int.parse(lesson["giorno"]) - 1];
-  //       if (currentDayName != dayName) {
-  //         currentDayName = dayName;
-  //         l.add(
-  //           new Text(
-  //             dayName,
-  //             style: GoogleFonts.cinzelDecorative(
-  //               textStyle: TextStyle(
-  //                   // fontWeight: FontWeight.bold,
-  //                   fontSize: 25,
-  //                   color: Theme.of(context).primaryColor),
-  //             ),
-  //             textAlign: TextAlign.center,
-  //           ),
-  //         );
-  //       }
-  //
-  //       l.add(
-  //         new LessonCard(
-  //           lesson["nome_insegnamento"],
-  //           lesson["docente"],
-  //           lesson["aula"],
-  //           lesson["ora_inizio"],
-  //           lesson["ora_fine"],
-  //           lesson["extra"],
-  //           widget._now,
-  //           false,
-  //         ),
-  //       );
-  //     }
-  //   });
-  //   setState(() {
-  //     this._lessonsWidgets = l;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
-    // _lessonsWidgets = [];
     String currentDayName = "";
 
-    // return new ListView.builder(
-    //     itemCount: _lessonsWidgets.length,
-    //     itemBuilder: (context, index) => _lessonsWidgets[index],
-    // );
+    int numElements = widget._lessons.length + nDays;
+    int currIndex = 0;
     return this.nDays == -1
         ? Loading()
         : new ListView.builder(
-            itemCount: widget._lessons.length + nDays,
+            itemCount: numElements,
             itemBuilder: (context, index) {
-              var lesson = widget._lessons[index];
-              String dayName =
-                  widget._nomeGiorni[int.parse(lesson["giorno"]) - 1];
+              var lesson = widget._lessons[currIndex];
+              String dayName = widget._nomeGiorni[int.parse(lesson["giorno"]) - 1];
 
               if (currentDayName != dayName) {
                 currentDayName = dayName;
@@ -156,18 +111,19 @@ class _WeekViewState extends State<WeekView> {
                   ),
                   textAlign: TextAlign.center,
                 );
+              } else {
+                currIndex++;
+                return new LessonCard(
+                  lesson["nome_insegnamento"],
+                  lesson["docente"],
+                  lesson["aula"],
+                  lesson["ora_inizio"],
+                  lesson["ora_fine"],
+                  lesson["extra"],
+                  widget._now,
+                  false,
+                );
               }
-
-              return new LessonCard(
-                lesson["nome_insegnamento"],
-                lesson["docente"],
-                lesson["aula"],
-                lesson["ora_inizio"],
-                lesson["ora_fine"],
-                lesson["extra"],
-                widget._now,
-                false,
-              );
             },
           );
   }
